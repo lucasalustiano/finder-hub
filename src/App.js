ab0b5429cb20236abaad7e0f8b27938fb10a64fb
lucasalustiano <lucas.salustiano@ccc.ufcg.edu.br>
2022-02-04 08:45:01 -0300

    create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routede } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
